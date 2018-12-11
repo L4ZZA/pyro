a6838a255c3fbecc588f753cefe0fc2430c2f7e7
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-12-11 15:05:04 +0000

    Created pyro wrapper of spdlog

---
+++ Pyro/src/pyro/logger.cpp
@@ -0,0 +1,14 @@
+﻿#include "logger.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+std::shared_ptr<spdlog::logger> pyro::logger::s_core_logger;
+std::shared_ptr<spdlog::logger> pyro::logger::s_client_logger;
+
+void pyro::logger::init()
+{
+    spdlog::set_pattern("%^[%T] %n: %v%$");
+    s_core_logger = spdlog::stdout_color_mt("PYRO");
+    s_core_logger->set_level(spdlog::level::trace);
+    s_client_logger = spdlog::stdout_color_mt("APP");
+    s_client_logger->set_level(spdlog::level::trace);
+}
