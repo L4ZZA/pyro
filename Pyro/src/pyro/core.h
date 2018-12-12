c48ae14cf3a16dced9b18038ecf6e9aac3df74b2
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-12-12 01:11:55 +0000

    Added engine asserts

--- Pyro/src/pyro/core.h
+++ Pyro/src/pyro/core.h
@@ -17,3 +17,12 @@
 // 1 << 1 = `0000 0010`
 // 1 << 2 = `0000 0100`
 #define BIT(x) 1 << x
+
+
+#ifdef PYRO_ENABLE_ASSERTS
+    #define PYRO_ASSERT(x, ...) { if(!x) {PYRO_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak();}}
+    #define PYRO_CORE_ASSERT(x, ...) { if(!x) {PYRO_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak();}}
+#else
+    #define PYRO_ASSERT(x, ...)
+    #define PYRO_CORE_ASSERT(x, ...)
+#endif
