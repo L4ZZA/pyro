9458e60bfb958ae16035d557b39b3c9f165b27b7
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-01-07 15:46:25 +0000

    Added layer interface

---
+++ Pyro/src/pyro/layer.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "pyro/core.h"
+#include "pyro/events/event.h"
+
+namespace pyro
+{
+
+    class PYRO_API layer
+    {
+    public:
+        layer(const std::string &p_name = "Layer");
+        virtual ~layer();
+
+        virtual void on_attach() {}
+        virtual void on_detach() {}
+        virtual void on_update() {}
+        virtual void on_event(event &p_event) {}
+
+        inline const std::string& name() const { return m_debug_name; }
+
+    protected:
+        std::string m_debug_name;
+    };
+}
