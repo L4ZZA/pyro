fed7c3998df1ac1a40a0f841f6162b86deb794fe
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-01-07 21:19:50 +0000

    generated project and added igmui layer

---
+++ Pyro/src/pyro/imgui/imgui_layer.cpp
@@ -0,0 +1,19 @@
+﻿#include "pyro_pch.h"
+#include "imgui_layer.h"
+
+pyro::imgui_layer::imgui_layer()
+    : layer("imgui_layer")
+{
+}
+
+pyro::imgui_layer::~imgui_layer()
+{
+}
+
+void pyro::imgui_layer::on_update()
+{
+}
+
+void pyro::imgui_layer::on_event(event& p_event)
+{
+}
---
+++ Pyro/src/pyro/imgui/imgui_layer.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "pyro/layer.h"
+
+namespace pyro
+{
+    class imgui_layer : public pyro::layer
+    {
+    public:
+        imgui_layer();
+        ~imgui_layer();
+
+        void on_update() override;
+        void on_event(event& p_event) override;
+
+    private:
+
+
+    };
+}
