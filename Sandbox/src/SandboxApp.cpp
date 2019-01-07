922c5d99f31a6b805951f96b0c7175c2c0312e92
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-01-07 16:54:05 +0000

    example layer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,29 @@
 #include <pyro.h>
 
+class ExampleLayer : public pyro::layer
+{
+public:
+    ExampleLayer() : layer("example") {  }
+
+    void on_update() override
+    {
+        PYRO_INFO("ExampleLayer::on_update()");
+    }
+
+    void on_event(pyro::event& p_event) override
+    {
+        PYRO_TRACE("{0}", p_event);
+    }
+
+};
+
+
 class SandboxApp : public pyro::application
 {
     public:
     SandboxApp()
     {
-        
+        push_layer(new ExampleLayer());
     }
 
     ~SandboxApp()
