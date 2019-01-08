93ea14b8de8cf1bc159030e733f75831ab3439e4
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-01-08 11:46:33 +0000

    setup imgui to render from a pyro layer

--- Pyro/src/pyro/imgui/imgui_layer.cpp
+++ Pyro/src/pyro/imgui/imgui_layer.cpp
@@ -1,6 +1,10 @@
 ﻿#include "pyro_pch.h"
 #include "imgui_layer.h"
 
+#include "platform/opengl/imgui_opengl_renderer.h"
+#include "GLFW/glfw3.h"
+#include "pyro/application.h"
+
 pyro::imgui_layer::imgui_layer()
     : layer("imgui_layer")
 {
@@ -10,8 +14,65 @@ pyro::imgui_layer::~imgui_layer()
 {
 }
 
+void pyro::imgui_layer::on_attach()
+{
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
+    io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
+
+    // TEMPORARY!! Should eventually use pyro keycode.
+    // Keyboard mapping. ImGui will use those indices to peek into the io.KeysDown[] array.
+    io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
+    io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
+    io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+    io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
+    io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
+    io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
+    io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
+    io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
+    io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
+    io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
+    io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+    io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+    io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
+    io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+    io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
+    io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
+    io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
+    io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
+    io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
+    io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
+
+    ImGui_ImplOpenGL3_Init("#version 410");
+}
+
+void pyro::imgui_layer::on_detach()
+{
+}
+
 void pyro::imgui_layer::on_update()
 {
+
+    ImGuiIO& io = ImGui::GetIO();
+    auto& app = application::instance();
+    io.DisplaySize = ImVec2(app.get_window().width(), app.get_window().height());
+
+    float time = glfwGetTime();
+    io.DeltaTime = m_time > 0.0 ? (time - m_time) : (1.f / 60.f);
+    m_time = time;
+
+    // NewFrame after GetIO() and maybe also after io.DetltaTime
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
+
+    static bool show = true;
+    ImGui::ShowDemoWindow(&show);
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void pyro::imgui_layer::on_event(event& p_event)
--- Pyro/src/pyro/imgui/imgui_layer.h
+++ Pyro/src/pyro/imgui/imgui_layer.h
@@ -3,16 +3,20 @@
 
 namespace pyro
 {
-    class imgui_layer : public pyro::layer
+    class PYRO_API imgui_layer : public pyro::layer
     {
     public:
         imgui_layer();
         ~imgui_layer();
 
+        void on_attach() override;
+        void on_detach() override;
+
         void on_update() override;
         void on_event(event& p_event) override;
 
     private:
+        float m_time{ 0.f };
 
 
     };
