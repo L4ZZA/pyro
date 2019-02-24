b85dc9edc37ca13fff1cc11bb2f08f0961680f12
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-02-24 13:04:01 +0000

    updated imgui classes to use docking
    and to not have any major change so that we can just update the docking branch without having major refactoring every time

--- Pyro/src/pyro/imgui/imgui_layer.h
+++ Pyro/src/pyro/imgui/imgui_layer.h
@@ -13,20 +13,12 @@ namespace pyro
         imgui_layer();
         ~imgui_layer();
 
-        void on_attach() override;
-        void on_detach() override;
+        virtual void on_attach() override;
+        virtual void on_detach() override;
+        virtual void on_imgui_render() override;
 
-        void on_update() override;
-        void on_event(event& p_event) override;
-
-        bool on_mouse_button_pressed_event(mouse_button_pressed_event &e);
-        bool on_mouse_button_released_event(mouse_button_released_event &e);
-        bool on_mouse_moved_event(mouse_moved_event &e);
-        bool on_mouse_scrolled_event(mouse_scrolled_event &e);
-        bool on_key_pressed_event(key_pressed_event &e);
-        bool on_key_released_event(key_released_event &e);
-        bool on_key_typed_event(key_typed_event &e);
-        bool on_window_resize_event(window_resize_event &e);
+        void begin() const;
+        void end() const;
 
     private:
         float m_time{ 0.f };
