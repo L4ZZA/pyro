c6ba5c9b504462cf773290262774b647f930c928
L4ZZA <pietro.lazz@gmail.com>
2019-06-08 23:19:27 +0100

    Colored vertices based on position

--- Pyro/src/pyro/application.cpp
+++ Pyro/src/pyro/application.cpp
@@ -42,9 +42,11 @@ pyro::application::application()
         #version 430
 
         layout(location = 0) in vec3 a_position;
+        out vec3 v_position;
 
         void main()
         {
+            v_position = a_position;
             gl_Position = vec4(a_position, 1.0);
         }
     )";
@@ -53,10 +55,11 @@ pyro::application::application()
         #version 430
 
         layout(location = 0) out vec4 o_color;
+        in vec3 v_position;
 
         void main()
         {
-            o_color = vec4(0.8, 0.2, 0.3, 1.0);
+            o_color = vec4(v_position * 0.5 + 0.5, 1.0);
         }
     )";
 
