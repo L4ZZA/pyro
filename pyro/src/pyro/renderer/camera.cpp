09d14c34745737c128d5029e7474ae07a1348ba6
L4ZZA <pietro.lazz@gmail.com>
2019-07-16 23:01:23 +0100

    Created orthographic camera and other empty placeholders

---
+++ pyro/src/pyro/renderer/camera.cpp
@@ -0,0 +1,19 @@
+﻿#include "pyro_pch.h"
+#include "camera.h"
+#include "glm/gtc/matrix_transform.hpp"
+
+pyro::orthographic_camera::orthographic_camera(float left, float right, float bottom, float top)
+    : m_projection_mat(glm::ortho(left, right, bottom, top, -1.f, 1.f))
+{
+    m_view_projection_mat = m_projection_mat * m_view_mat;
+}
+
+void pyro::orthographic_camera::update_view_matrix()
+{
+    glm::mat4 transform = glm::translate(glm::mat4{1}, m_position) * 
+        glm::rotate(glm::mat4(1), m_rotation, glm::vec3(0,0,1));
+
+    // inverting the transform matrix 
+    m_view_mat = glm::inverse(transform);
+    m_view_projection_mat = m_projection_mat * m_view_mat;
+}
---
+++ pyro/src/pyro/renderer/camera.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#define GLM_FORCE_CTOR_INIT
+#include "glm/glm.hpp"
+
+namespace pyro
+{
+    class camera
+    {
+    public:
+
+    };
+
+    class orthographic_camera
+    {
+    public:
+        orthographic_camera(float left, float right, float bottom, float top);
+
+        glm::vec3 position() const { return m_position; }
+        void position(const glm::vec3& pos) { m_position = pos; update_view_matrix(); }
+
+        float rotation() const { return m_rotation; }
+        void rotation(float rot) { m_rotation = rot; update_view_matrix(); }
+
+        const glm::mat4& projection_matrix() const { return m_projection_mat; }
+        const glm::mat4& view_matrix() const { return m_view_mat; }
+        const glm::mat4& view_projection_matrix() const { return m_view_projection_mat; }
+
+    private:
+        void update_view_matrix();
+
+    private:
+        glm::mat4   m_projection_mat{ 1 };
+        glm::mat4   m_view_mat{ 1 };
+        glm::mat4   m_view_projection_mat{ 1 };
+
+        glm::vec3   m_position{ 0 };
+        float       m_rotation{ 0 };
+    };
+
+    class perspective_camera
+    {
+    public:
+
+    };
+}
