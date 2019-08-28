7b96a764de1d3965b3549bb82fe0a9a97ebde9c2
L4ZZA <pietro.lazz@gmail.com>
2019-08-28 22:19:38 +0100

    implemented 3d camera

--- pyro/src/pyro/renderer/camera.cpp
+++ pyro/src/pyro/renderer/camera.cpp
@@ -3,6 +3,7 @@
 #include "glm/gtc/matrix_transform.hpp"
 
 
+//================== Orthographic Camera [2D] =================================
 
 pyro::orthographic_camera::orthographic_camera(float left, float right, float bottom, float top)
     : m_projection_mat(glm::ortho(left, right, bottom, top, -1.f, 1.f))
@@ -45,10 +46,114 @@ void pyro::orthographic_camera::rotate(e_rotation rotation, timestep ts)
 
 void pyro::orthographic_camera::update_view_matrix()
 {
-    glm::mat4 transform = glm::translate(glm::mat4{1}, m_position) * 
-        glm::rotate(glm::mat4(1), glm::radians(m_rotation), glm::vec3(0,0,1));
+    glm::mat4 transform(1);
+    glm::rotate(transform, glm::radians(m_rotation), glm::vec3(0,0,1));
+    transform = glm::translate(transform, m_position);
 
     // inverting the transform matrix 
     m_view_mat = glm::inverse(transform);
     m_view_projection_mat = m_projection_mat * m_view_mat;
 }
+
+//================== Perspective Camera [3D] ==================================
+
+pyro::perspective_camera::perspective_camera( 
+    float width, float height, 
+    const glm::vec3& start_position /*= {0.f,0.f,3.f}*/, 
+    float fov /*= 45.f*/, 
+    float near_z /*= 0.1f*/, float far_z /*= 100.f*/) 
+    : m_projection_mat(glm::perspective(glm::radians(fov), width / height, near_z, far_z)), 
+    m_position(start_position), 
+    m_aspect_ratio(width / height), 
+    m_fov(fov), 
+    m_near_cplane(near_z), 
+    m_far_cplane(far_z) 
+{ 
+    // note that we're translating the scene in the reverse direction of where we want to move 
+    m_view_mat = translate(m_view_mat, m_position * -1.f); 
+    m_view_projection_mat = m_projection_mat * m_view_mat; 
+    PYRO_CORE_TRACE("3d cam position: [{},{},{}]", m_position.x, m_position.y, m_position.z); 
+    PYRO_CORE_TRACE("3d cam rotation: [{},{},{}]", m_rotation_angle.x, m_rotation_angle.y, m_rotation_angle.z); 
+} 
+
+const glm::mat4& pyro::perspective_camera::projection_matrix() const 
+{ 
+    return m_projection_mat; 
+} 
+
+const glm::mat4& pyro::perspective_camera::view_matrix() const 
+{ 
+    return m_view_mat; 
+} 
+
+const glm::mat4& pyro::perspective_camera::view_projection_matrix() const 
+{ 
+    return m_view_projection_mat; 
+} 
+
+void pyro::perspective_camera::move(e_direction direction, timestep ts) 
+{ 
+    if(direction == front) 
+        m_position.z += s_movement_speed * ts; 
+    else if(direction == back) 
+        m_position.z -= s_movement_speed * ts; 
+
+    if(direction == left) 
+        m_position.x -= s_movement_speed * ts; 
+    else if(direction == right) 
+        m_position.x += s_movement_speed * ts; 
+
+    PYRO_CORE_TRACE("3d cam position: [{},{},{}]", m_position.x, m_position.y, m_position.z); 
+
+    update_view_matrix(); 
+} 
+
+void pyro::perspective_camera::rotate(e_rotation rotation, e_axis rotation_axis, timestep ts) 
+{ 
+    float* angle_ref = nullptr; 
+    switch (rotation_axis) 
+    { 
+        case x: angle_ref = &m_rotation_angle.x; break; 
+        case y: angle_ref = &m_rotation_angle.y; break; 
+        case z: angle_ref = &m_rotation_angle.z; break; 
+    } 
+
+    PYRO_ASSERT(angle_ref, "angle ptr is null!") 
+
+        float& angle = *angle_ref; 
+    if(rotation == anticlock_wise) 
+    { 
+        angle += s_rotation_speed * ts; 
+        if(angle > 360.f) 
+        { 
+            angle = 0.f; 
+            PYRO_CORE_TRACE("A"); 
+        } 
+    } 
+    else if(rotation == clock_wise) 
+    { 
+        angle -= s_rotation_speed * ts; 
+        if(angle < 0.f) 
+        { 
+            angle = 360.f; 
+            PYRO_CORE_TRACE("B"); 
+        } 
+    } 
+
+    PYRO_CORE_TRACE("after - 3d cam rotation: [{},{},{}]", m_rotation_angle.x, m_rotation_angle.y, m_rotation_angle.z); 
+
+    update_view_matrix(); 
+} 
+
+void pyro::perspective_camera::update_view_matrix() 
+{ 
+    glm::mat4 transform{1}; 
+    transform = glm::rotate(transform, glm::radians(m_rotation_angle.x), {1,0,0}); 
+    transform = glm::rotate(transform, glm::radians(m_rotation_angle.y), {0,1,0}); 
+    transform = glm::rotate(transform, glm::radians(m_rotation_angle.z), {0,0,1}); 
+    transform = glm::translate(transform, m_position); 
+
+    // inverting the transform matrix  
+    m_view_mat = glm::inverse(transform); 
+    m_view_projection_mat = m_projection_mat * m_view_mat; 
+} 
--- pyro/src/pyro/renderer/camera.h
+++ pyro/src/pyro/renderer/camera.h
@@ -9,19 +9,18 @@ namespace pyro
     class camera
     {
     public:
-        enum e_direction
-        {
-            up = 0,
-            down,
-            left,
-            right
-        };
-
         enum e_rotation
         {
             clock_wise = 0,
             anticlock_wise
-        };
+        }; 
+
+        enum e_axis 
+        { 
+            x = 0, 
+            y, 
+            z 
+        }; 
 
     public:
         virtual ~camera() = default;
@@ -31,13 +30,19 @@ namespace pyro
         virtual const glm::mat4& view_projection_matrix() const = 0;
         virtual float movement_speed() const = 0;
         virtual float rotation_speed() const = 0;
-
-        virtual void move(e_direction direction, timestep ts) = 0;
-        virtual void rotate(e_rotation rotation, timestep ts) = 0;
     };
 
     class orthographic_camera final : public camera
     {
+    public: 
+        enum e_direction 
+        { 
+            up = 0, 
+            down, 
+            left, 
+            right 
+        }; 
+
     public:
         orthographic_camera(float left, float right, float bottom, float top);
 
@@ -54,8 +59,8 @@ namespace pyro
         float movement_speed() const override { return s_movement_speed; }
         float rotation_speed() const override { return s_rotation_speed; }
 
-        void move(e_direction direction, timestep ts) override;
-        void rotate(e_rotation rotation, timestep ts) override;
+        void move(e_direction direction, timestep ts);
+        void rotate(e_rotation rotation, timestep ts);
 
     private:
         void update_view_matrix();
@@ -74,9 +79,57 @@ namespace pyro
         inline static const float s_rotation_speed = 180.0f;
     };
 
-    class perspective_camera
+    class perspective_camera : public camera
     {
     public:
+        enum e_direction 
+        { 
+            front = 0, 
+            back, 
+            left, 
+            right 
+        };
 
+    public: 
+        perspective_camera( 
+            float width, float height, 
+            const glm::vec3& start_position = {0.f,0.f,3.f},  
+            float fov = 45.f,  
+            float near_z = 0.1f, float far_z = 100.f); 
+        const glm::mat4& projection_matrix() const override; 
+        const glm::mat4& view_matrix() const override; 
+        const glm::mat4& view_projection_matrix() const override; 
+
+        float movement_speed() const override { return s_movement_speed; } 
+        float rotation_speed() const override { return s_rotation_speed; } 
+
+        void move(e_direction direction, timestep ts); 
+        void rotate(e_rotation rotation, e_axis rotation_axis, timestep ts);
+
+    private: 
+        void update_view_matrix(); 
+
+    private: 
+        glm::mat4   m_projection_mat{1}; 
+        glm::mat4   m_view_mat{1}; 
+        glm::mat4   m_view_projection_mat{1}; 
+
+        glm::vec3   m_position{0}; 
+        /// \brief rotation angles for each axis in degrees. 
+        glm::vec3   m_rotation_angle{0.f}; 
+
+        /// \brief 
+        float m_aspect_ratio{1}; 
+        /// \brief Field of view in degrees. 
+        float m_fov{45.f}; 
+        /// \brief Near clipping plane. 
+        float m_near_cplane{0.1f}; 
+        /// \brief ar clipping plane. 
+        float m_far_cplane{100.f}; 
+
+        /// \brief in units per seconds. 
+        inline static const float s_movement_speed = 1.f; 
+        /// \brief in degrees per second. 
+        inline static const float s_rotation_speed = 90.f;
     };
 }
