9e531d94deb05d39be90d244b03b11eb06b740a1
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-09-04 18:05:07 +0100

    Made input a true singleton

--- pyro/src/pyro/input.h
+++ pyro/src/pyro/input.h
@@ -5,16 +5,21 @@
 namespace pyro
 {
     /// \brief Base class for input polling.
-    /// Input polling allows to retrieve input states at any time.
+    /// input polling allows to retrieve input states at any time.
     class PYRO_API input
     {
+    protected:
+        input() = default;
     public:
+        input(const input&) = delete;
+        input& operator=(const input&) = delete;
+
         /// \brief Returns if the specified key is being pressed.
         static bool key_pressed(int p_key_code) { return s_instance->key_pressed_impl(p_key_code); }
-        
+
         /// \brief Returns if the specified mouse button is being pressed.
         static bool mouse_button_pressed(int p_button) { return s_instance->mouse_button_pressed_impl(p_button); }
-        
+
         /// \brief Returns the mouse position as a pair of float values.
         static std::pair<float, float> mouse_position() { return s_instance->mouse_position_impl(); }
         /// \brief Returns the x coordinate of the mouse position.
