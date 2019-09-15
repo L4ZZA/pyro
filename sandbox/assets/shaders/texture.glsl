4e0415889d3de7eebf666152c158928a3e9d3150
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-09-15 21:51:31 +0100

    nitpicking

--- sandbox/assets/shaders/texture.glsl
+++ sandbox/assets/shaders/texture.glsl
@@ -1,33 +1,32 @@
-
-///
+// Basic Texture Shader
 
 #type vertex
-#version 430  
-  
-layout(location = 0) in vec3 a_position;  
-layout(location = 1) in vec2 a_tex_coord;  
-  
-uniform mat4 u_view_projection;  
-uniform mat4 u_transform;  
-  
-out vec2 v_tex_coord;   
-  
-void main()  
-{  
-    v_tex_coord = a_tex_coord;  
-    gl_Position = u_view_projection * u_transform * vec4(a_position, 1.0);  
-}  
+#version 430
+
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec2 a_tex_coord;
+
+uniform mat4 u_view_projection;
+uniform mat4 u_transform;
+
+out vec2 v_tex_coord;
+
+void main()
+{
+    v_tex_coord = a_tex_coord;
+    gl_Position = u_view_projection * u_transform * vec4(a_position, 1.0);
+}
 
 #type fragment
-#version 430  
-  
-layout(location = 0) out vec4 o_color;  
-  
-in vec2 v_tex_coord;  
-  
-uniform sampler2D u_sampler;   
-  
-void main()  
-{  
-    o_color = texture(u_sampler, v_tex_coord); 
+#version 430
+
+layout(location = 0) out vec4 o_color;
+
+in vec2 v_tex_coord;
+
+uniform sampler2D u_sampler;
+
+void main()
+{
+    o_color = texture(u_sampler, v_tex_coord);
 }  
