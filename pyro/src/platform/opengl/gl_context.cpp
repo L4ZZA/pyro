865223093ddaaa91ac23e7dc31bb0187510c70f1
L4ZZA <pietro.lazz@gmail.com>
2019-10-12 16:50:42 +0100

    Added opengl version assertion

--- pyro/src/platform/opengl/gl_context.cpp
+++ pyro/src/platform/opengl/gl_context.cpp
@@ -16,9 +16,18 @@ void pyro::gl_context::init()
 	PYRO_CORE_ASSERT(status, "Could not load Glad!");
 
 	PYRO_CORE_INFO("OpenGL Info:");
-	PYRO_CORE_INFO("\tVendor: {}", glGetString(GL_VENDOR));
+	PYRO_CORE_INFO("\tVendor: {}",   glGetString(GL_VENDOR));
 	PYRO_CORE_INFO("\tRenderer: {}", glGetString(GL_RENDERER));
-	PYRO_CORE_INFO("\tVersion: {}", glGetString(GL_VERSION));
+	PYRO_CORE_INFO("\tVersion: {}",  glGetString(GL_VERSION));
+    
+	#ifdef PYRO_ENABLE_ASSERTS
+		int version_major;
+		int version_minor;
+		glGetIntegerv(GL_MAJOR_VERSION, &version_major);
+		glGetIntegerv(GL_MINOR_VERSION, &version_minor);
+
+		PYRO_CORE_ASSERT(version_major > 4 || (version_major == 4 && version_minor >= 5), "Pyro requires at least OpenGL version 4.5!");
+	#endif
 }
 
 void pyro::gl_context::swap_buffers()
