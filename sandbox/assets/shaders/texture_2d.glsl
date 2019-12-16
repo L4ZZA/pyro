4bc47fba82ac98cc5503c8b33821542b1e7d281c
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-12-16 15:40:47 +0000

    Prepared for vignette effect

--- sandbox/assets/shaders/texture_2d.glsl
+++ sandbox/assets/shaders/texture_2d.glsl
@@ -12,11 +12,13 @@ uniform mat4 u_view_projection;
 uniform mat4 u_transform;
 
 out vec2 v_tex_coord;
+//out vec2 v_screen_pos;
 
 void main()
 {
     v_tex_coord = a_tex_coord;
     gl_Position = u_view_projection * u_transform * vec4(a_position, 1.0);
+	//v_screen_pos = gl_Position.xy;
 }
 
 #type fragment
@@ -25,10 +27,19 @@ void main()
 layout(location = 0) out vec4 o_color;
 
 in vec2 v_tex_coord;
+//in vec2 v_screen_pos;
 
 uniform vec4 u_color;  
 uniform float u_tiling_factor;  
 uniform sampler2D u_sampler; 
+
+float vignette(vec2 screen_pos, float radius)
+{
+	float distance = 1.0f - distance(screen_pos * radius, vec2(0.0f));
+	distance = clamp(distance, 0.f, 1.f);
+	distance = sqrt(distance);
+	return distance;
+}
 
 void main() 
 { 
