b3c30874f35458f69cbfcf47c52988f36dd6dd17
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-12-18 13:58:08 +0000

    added framebuffer implementation

---
+++ pyro/src/pyro/renderer/frame_buffer_2d.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "pyro/renderer/frame_buffer.h"
+
+namespace pyro
+{
+    class frame_buffer_2d : public frame_buffer
+    {
+    public:
+        virtual void clear_color(glm::vec4 const &color) = 0;
+
+    public:
+        static ref<frame_buffer_2d> create(uint32_t width, uint32_t height);
+    };
+}
