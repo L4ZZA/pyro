13d5606fa20952a6bcc3cfb006db9ddaa1269dfd
L4ZZA <pietro.lazz@gmail.com>
2020-05-02 13:45:05 +0100

    added random generator helper

---
+++ sandbox/src/helpers/random.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <random>
+
+class random
+{
+public:
+	static void init()
+	{
+		s_random_engine.seed(std::random_device()());
+	}
+
+	// Returns a random floating point value between zero and uint32::max
+	static float get_float()
+	{
+		return static_cast<float>(s_distribution(s_random_engine)) / static_cast<float>(std::numeric_limits<uint32_t>::max());
+	}
+
+private:
+	inline static std::mt19937 s_random_engine;
+	inline static std::uniform_int_distribution<std::mt19937::result_type> s_distribution;
+};
