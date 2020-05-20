22a04792b6280ec6535009761fb71a05ea4b5047
L4ZZA <pietro.lazz@gmail.com>
2020-05-20 14:21:49 +0100

    Introduced log level and conditional logging

--- pyro/src/pyro/core/logger.cpp
+++ pyro/src/pyro/core/logger.cpp
@@ -5,6 +5,16 @@
 pyro::ref<spdlog::logger> pyro::logger::s_core_logger;
 pyro::ref<spdlog::logger> pyro::logger::s_client_logger;
 
+enum class pyro::logger::e_level
+{
+    debug   = (int)spdlog::level::trace,
+    info    = (int)spdlog::level::info,
+    warning = (int)spdlog::level::warn,
+    error   = (int)spdlog::level::err,
+    severe  = (int)spdlog::level::critical,
+    off     = (int)spdlog::level::off,
+};
+
 void pyro::logger::init()
 {
     spdlog::set_pattern("%^[%T] %n: %v%$");
@@ -12,4 +22,10 @@ void pyro::logger::init()
     s_core_logger->set_level(spdlog::level::trace);
     s_client_logger = spdlog::stdout_color_mt("APP");
     s_client_logger->set_level(spdlog::level::trace);
+}
+
+void pyro::logger::log_level(e_level const& level)
+{
+    s_core_logger->set_level(static_cast<spdlog::level::level_enum>(level));
+    s_client_logger->set_level(static_cast<spdlog::level::level_enum>(level));
 }
--- pyro/src/pyro/core/logger.h
+++ pyro/src/pyro/core/logger.h
@@ -9,7 +9,11 @@ namespace pyro
     class PYRO_API logger
     {
     public:
+        enum class e_level;
+
+    public:
         static void init();
+        static void log_level(e_level const& level);
         static std::shared_ptr<spdlog::logger> const& core_logger() { return s_core_logger; }
         static std::shared_ptr<spdlog::logger> const& client_logger() { return s_client_logger; }
     private:
@@ -18,15 +22,34 @@ namespace pyro
     };
 }
 
+#ifdef PYRO_DEBUG
+
+    // Core log macros
+    #define PYRO_CORE_TRACE(...)    ::pyro::logger::core_logger()->trace(__VA_ARGS__)
+    #define PYRO_CORE_INFO(...)     ::pyro::logger::core_logger()->info(__VA_ARGS__)
+    #define PYRO_CORE_WARN(...)     ::pyro::logger::core_logger()->warn(__VA_ARGS__)
+    #define PYRO_CORE_ERROR(...)    ::pyro::logger::core_logger()->error(__VA_ARGS__)
+    #define PYRO_CORE_FATAL(...)    ::pyro::logger::core_logger()->critical(__VA_ARGS__)
+     // Client log macros
+    #define PYRO_TRACE(...)	      ::pyro::logger::client_logger()->trace(__VA_ARGS__)
+    #define PYRO_INFO(...)	      ::pyro::logger::client_logger()->info(__VA_ARGS__)
+    #define PYRO_WARN(...)	      ::pyro::logger::client_logger()->warn(__VA_ARGS__)
+    #define PYRO_ERROR(...)	      ::pyro::logger::client_logger()->error(__VA_ARGS__)
+    #define PYRO_FATAL(...)	      ::pyro::logger::client_logger()->critical(__VA_ARGS__)
+
+#else
+
 // Core log macros
-#define PYRO_CORE_TRACE(...)    ::pyro::logger::core_logger()->trace(__VA_ARGS__)
-#define PYRO_CORE_INFO(...)     ::pyro::logger::core_logger()->info(__VA_ARGS__)
-#define PYRO_CORE_WARN(...)     ::pyro::logger::core_logger()->warn(__VA_ARGS__)
-#define PYRO_CORE_ERROR(...)    ::pyro::logger::core_logger()->error(__VA_ARGS__)
-#define PYRO_CORE_FATAL(...)    ::pyro::logger::core_logger()->critical(__VA_ARGS__)
+#define PYRO_CORE_TRACE(...)
+#define PYRO_CORE_INFO(...) 
+#define PYRO_CORE_WARN(...) 
+#define PYRO_CORE_ERROR(...)
+#define PYRO_CORE_FATAL(...)
  // Client log macros
-#define PYRO_TRACE(...)	      ::pyro::logger::client_logger()->trace(__VA_ARGS__)
-#define PYRO_INFO(...)	      ::pyro::logger::client_logger()->info(__VA_ARGS__)
-#define PYRO_WARN(...)	      ::pyro::logger::client_logger()->warn(__VA_ARGS__)
-#define PYRO_ERROR(...)	      ::pyro::logger::client_logger()->error(__VA_ARGS__)
-#define PYRO_FATAL(...)	      ::pyro::logger::client_logger()->critical(__VA_ARGS__)
+#define PYRO_TRACE(...)	    
+#define PYRO_INFO(...)	    
+#define PYRO_WARN(...)	    
+#define PYRO_ERROR(...)	    
+#define PYRO_FATAL(...)	 
+
+#endif 
