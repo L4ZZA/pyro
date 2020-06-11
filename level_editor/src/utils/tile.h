11e00304a8703b9642217097e131597cb6ed4400
L4ZZA <pietro.lazz@gmail.com>
2020-06-11 16:29:30 +0100

    Moved tile methods to board generator

--- level_editor/src/utils/tile.h
+++ level_editor/src/utils/tile.h
@@ -14,40 +14,4 @@ struct tile
     int y;
     e_tile_type type;
     pyro::ref<room> room_ptr;
-
-    bool is_floor(pyro::ref<room> r) const
-    {
-        return x > r->left && x < r->right &&
-            y > r->bottom && y < r->top;
-    }
-
-    bool is_wall(pyro::ref<room> r) const
-    {
-        const bool is_left_wall = x == r->left && y >= r->bottom && y <= r->top;
-        const bool is_right_wall = x == r->right && y >= r->bottom && y <= r->top;
-        const bool is_top_wall = y == r->top && x >= r->left && x <= r->right;
-        const bool is_bottom_wall = y == r->bottom && x >= r->left && x <= r->right;
-
-        return is_left_wall || is_right_wall || is_top_wall || is_bottom_wall;
-    }
-
-    bool is_door(pyro::ref<room> r) const
-    {
-        const bool is_in_door  = x == r->in_door.x && y == r->in_door.y;
-        const bool is_out_door = x == r->out_door.x && y == r->out_door.y;
-
-        return is_in_door || is_out_door;
-    }
-
-    bool is_in_door(pyro::ref<room> r) const
-    {
-        const bool is_in_door  = x == r->in_door.x && y == r->in_door.y;
-        return is_in_door;
-    }
-
-    bool is_out_door(pyro::ref<room> r) const
-    {
-        const bool is_out_door = x == r->out_door.x && y == r->out_door.y;
-        return is_out_door;
-    }
 };
