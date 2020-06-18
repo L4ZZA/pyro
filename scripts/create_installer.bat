387d6120b827771a111819f80fb780585a3bdbb2
L4ZZA <pietro.lazz@gmail.com>
2020-06-18 16:21:22 +0100

    Moved and updated scripts

--- scripts/create_installer.bat
+++ scripts/create_installer.bat
@@ -1,2 +1,5 @@
+@rem run following lines one folder above
+pushd %~dp0\..\ 
 call "C:\Program Files (x86)\NSIS\makensis.exe" "installer\LevelEditor.nsi"
+popd
 PAUSE
--- scripts/generate_solution_vs2019.bat
+++ scripts/generate_solution_vs2019.bat
@@ -1,5 +1,8 @@
 @echo off
+@rem run following lines one folder above
+pushd %~dp0\..\ 
 call vendor\premake\premake5.exe vs2019
+popd
 IF %ERRORLEVEL% NEQ 0 (
   PAUSE
 )
