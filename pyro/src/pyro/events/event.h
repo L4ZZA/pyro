d92dfa131144e3e7206f926bc3adecfdef9d29f7
L4ZZA <pietro.lazz@gmail.com>
2020-06-19 10:23:55 +0100

    Fixed event enums names

--- pyro/src/pyro/events/event.h
+++ pyro/src/pyro/events/event.h
@@ -8,7 +8,7 @@ namespace  pyro
 
     //=========================================================================
 
-    enum class event_type_e
+    enum class e_event_type
     {
         none = 0,
         window_close, window_resize, window_focus, window_lost_focus, window_moved,
@@ -19,7 +19,7 @@ namespace  pyro
 
     //-------------------------------------------------------------------------
 
-    enum event_category_e
+    enum e_event_category
     {
         none = 0,
         event_category_application  = BIT(0),
@@ -31,8 +31,8 @@ namespace  pyro
 
     //-------------------------------------------------------------------------
 
-#define EVENT_CLASS_TYPE(type) static event_type_e static_type() { return event_type_e::type; }\
-                                virtual event_type_e event_type() const override { return static_type(); }\
+#define EVENT_CLASS_TYPE(type) static e_event_type static_type() { return e_event_type::type; }\
+                                virtual e_event_type event_type() const override { return static_type(); }\
                                 virtual const char* name() const override { return #type; }
 
 #define EVENT_CLASS_CATEGORY(category) virtual int category_flags() const override { return category; }
@@ -48,7 +48,7 @@ namespace  pyro
 
     public:
         virtual ~event() = default;
-        virtual event_type_e event_type() const = 0;
+        virtual e_event_type event_type() const = 0;
         virtual const char* name() const = 0;
         virtual int category_flags() const = 0;
         virtual std::string to_string() const { return name(); }
@@ -56,7 +56,7 @@ namespace  pyro
         ///Tells if event is in the given category.
         ///\param category 
         ///\return 0 (false) if not in the category, true elsewhere
-        bool is_in_category(event_category_e category) const
+        bool is_in_category(e_event_category category) const
         {
             return category_flags() & category;
         }
