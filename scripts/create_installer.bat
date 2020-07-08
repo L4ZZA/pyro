84f59e168f1e6b293d6f9441cceec8aec9d2ee74
L4ZZA <pietro.lazz@gmail.com>
2020-07-08 15:35:26 +0100

    Fixed scripts starting directory

--- scripts/create_installer.bat
+++ scripts/create_installer.bat
@@ -1,5 +1,5 @@
 @rem run following lines one folder above
-pushd %~dp0\..\ 
+pushd %~dp0..\ 
 call "C:\Program Files (x86)\NSIS\makensis.exe" "installer\Ember.nsi"
 popd
 
--- scripts/generate_solution_vs2019.bat
+++ scripts/generate_solution_vs2019.bat
@@ -1,5 +1,5 @@
 @rem run following lines one folder above
-pushd %~dp0\..\ 
+pushd %~dp0..\ 
 call vendor\premake\premake5.exe vs2019
 popd
 IF %ERRORLEVEL% NEQ 0 (
