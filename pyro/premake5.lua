642d38364c56cea5aaa9296e4abfb8a8d85e54b6
L4ZZA <pietro.lazz@gmail.com>
2020-09-20 16:28:33 +0100

    modularised premake scripts

---
+++ pyro/premake5.lua
@@ -0,0 +1,72 @@
+
+project "pyro"
+
+kind "StaticLib" -- Static library (.lib)
+language "C++"
+cppdialect "C++17"
+staticruntime "on"
+
+targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+objdir ("%{wks.location}/inter/" .. outputdir .. "/%{prj.name}")
+
+pchheader "pyro_pch.h"
+pchsource "src/pyro_pch.cpp"
+
+files
+{
+    -- ** means recursively search down that folder
+    "src/**.h",
+    "src/**.cpp",
+    "external/glm/glm/**.hpp",
+    "external/glm/glm/**.inl",
+}
+
+defines
+{
+    "_CRT_SECURE_NO_WARNINGS",
+    "GLFW_INCLUDE_NONE"
+}
+
+includedirs
+{
+    "src",
+    "external/spdlog/include",
+    "%{IncludeDir.GLFW}",
+    "%{IncludeDir.Glad}",
+    "%{IncludeDir.ImGui}",
+    "%{IncludeDir.glm}",
+    "%{IncludeDir.stb_image}",
+    "%{IncludeDir.entt}",
+}
+
+links
+{
+    "GLFW",
+    "Glad",
+    "ImGui",
+    "opengl32.lib",
+    "stb_image",
+}
+
+-- filters are used to apply property to some specific configurations only
+filter "system:windows"
+    systemversion "latest" -- windows SDK version
+
+    defines
+    {
+    }
+
+filter "configurations:Debug"
+    defines "PYRO_DEBUG"
+    runtime "Debug"
+    symbols "on"
+
+filter "configurations:Release"
+    defines "PYRO_RELEASE"
+    runtime "Release"
+    optimize "on"
+
+filter "configurations:Dist"
+    defines "PYRO_DIST"
+    runtime "Release"
+    optimize "on"
---
+++ tests/premake5.lua
@@ -0,0 +1,56 @@
+
+project "tests"
+
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++17"
+staticruntime "on"
+
+targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+objdir ("%{wks.location}/inter/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+    "src/**.h",
+    "src/**.cpp"
+}
+
+includedirs
+{
+    "src",
+    "ember/src",
+    "pyro/external/spdlog/include",
+    "pyro/src",
+    "pyro/external",
+    "%{IncludeDir.glm}",
+    "%{IncludeDir.ImGui}",
+}
+
+links
+{
+    "ember",
+    "pyro",
+}
+
+filter "system:windows"
+    systemversion "latest"
+
+    defines
+    {
+        "NOMINMAX",
+    }
+
+filter "configurations:Debug"
+    defines "PYRO_DEBUG"
+    runtime "Debug"
+    symbols "on"
+
+filter "configurations:Release"
+    defines "PYRO_RELEASE"
+    runtime "Release"
+    optimize "on"
+
+filter "configurations:Dist"
+    defines "PYRO_DIST"
+    runtime "Release"
+    optimize "on"
