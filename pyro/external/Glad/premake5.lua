24947b105f70b7a569fdfe265aaeaa63faa4e0c6
L4ZZA <pietro.lazz@gmail.com>
2020-11-16 12:43:21 +0100

    Changed external projects output path

--- pyro/external/Glad/premake5.lua
+++ pyro/external/Glad/premake5.lua
@@ -3,8 +3,8 @@ project "Glad"
     language "C"
     staticruntime "on"
 
-    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("%{wks.location}/inter/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/external/%{prj.name}")
+    objdir ("%{wks.location}/inter/" .. outputdir .. "/external/%{prj.name}")
 
     files
     {
